13e88cc37140812e2076d6c65fc4cc60c5725754
Delzi <67564239+Delzi@users.noreply.github.com>
2023-08-24 06:23:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 const storyElement = document.getElementById("story");
 const choiceButton = document.getElementById("choice");
 
-let scene = 0;
-
-const scenes = [
-  "Welcome to the game! You find yourself in a mysterious forest.",
-  "You come across a fork in the path. Which way do you go?"
-  // Add more scenes and choices here
+let numEncounters = 0;
+let randomEncounter = Math.random();
+let inventory = [
+  "Item1" = false;
+  "Item2" = false;
 ];
 
+const encounters = [
+  "This is a random encounter; Goblins!",
+  "This is a random encounter; Monkeys!",
+  "This is a random encounter; Flowers!",
+];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 choiceButton.addEventListener("click", () => {
-  if (scene === 0) {
-    storyElement.textContent = "You pick the left path and discover a hidden treasure!";
+  randomEncounter = encounters[getRandomInt(encounters.length)]
+  
+    storyElement.textContent = randomEncounter;
     choiceButton.textContent = "Continue";
-  } else if (scene === 1) {
-    storyElement.textContent = "You come across a friendly gnome who offers to guide you.";
-    choiceButton.textContent = "Follow the gnome";
-  } else if (scene === 2) {
-    storyElement.textContent = "The gnome leads you to a magical portal. Do you enter?";
-    choiceButton.textContent = "Enter the portal";
-  } else if (scene === 3) {
-    storyElement.textContent = "You find yourself in a new realm filled with wonders!";
+
+
+  
+    numEncounters++;
+  if (numEncounters >= 5) {
+    storyElement.textContent = "The end!";
     choiceButton.style.display = "none"; // Hide the button to indicate the end
   }
-  scene++;
+  
 });
