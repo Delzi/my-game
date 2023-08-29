d741595694b236f378f2f5c388bda073198d84a7
Delzi <delzimazon@gmail.com>
2023-08-29 11:56:34 +0200

    Updated index.html and script.js

--- script.js
+++ script.js
@@ -3,25 +3,46 @@ const choiceButton = document.getElementById("choice");
 const requiredEncounters = 5;
 
 let numEncounters = 0;
+let lastEncounter = -1;
 let randomEncounter = Math.random();
 let inventory = {
-  "Item1": false,
-  "Item2": false
+	"Item1": false,
+	"Item2": false
 };
 
 const encounters = [
-  "This is a random encounter; Goblins!",
-  "This is a random encounter; Monkeys!",
-  "This is a random encounter; Flowers!",
+	"This is a random encounter; Goblins!",
+	"This is a random encounter; Monkeys!",
+	"This is a random encounter; Flowers!",
 ];
 
+function getRandom() {
+	if (lastEncounter >= 0) {
+		randomEncounter = encounters[Math.floor(Math.random() * encounters.length)];
+	} else {
+		for (let i = 0; randomEncounter != lastEncounter; i++) {
+			randomEncounter = encounters[Math.floor(Math.random() * encounters.length)];
+		}
+	}
+	return randomEncounter;
+};
+
 choiceButton.addEventListener("click", () => {
-  if (numEncounters < requiredEncounters) {
-    storyElement.textContent = encounters[Math.floor(Math.random() * encounters.length)];
-    choiceButton.textContent = "Continue";
-    numEncounters++;
-  } else {
-    storyElement.textContent = "The end!";
-    choiceButton.style.display = "none";
-  }
+	if (choiceButton.textContent === "Play again") {
+		numEncounters = 0;
+		for (let i = 0; i < inventory.length; i++) {
+			inventory[i] = false;
+		}
+	} else {
+		if (numEncounters < requiredEncounters) {
+			randomEncounter = genRandom();
+			lastEncounter = randomEncounter;
+			storyElement.textContent = encounters[randomEncounter];
+			choiceButton.textContent = "Continue";
+			numEncounters++;
+		} else {
+			storyElement.textContent = "The end!";
+			choiceButton.textContent = "Play again";
+		}
+	}
 });
